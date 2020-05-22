e004fd601e0af29613c1c3a97f4a33f70a830d4a
Sebastian Ariel zonta <seba_zonta@hotmail.com>
2020-05-22 12:24:29 -0300

    Programado metodos getall y getOne

--- Lab03/Business.Logic/UsuarioLogic.cs
+++ Lab03/Business.Logic/UsuarioLogic.cs
@@ -9,5 +9,33 @@ namespace Business.Logic
 {
     class UsuarioLogic:BusinessLogic
     {
+        private Data.Database.UsuarioAdapter _UsuarioData;
+
+        public Data.Database.UsuarioAdapter UsuarioData { get => _UsuarioData; set => _UsuarioData = value; }
+
+        public Business.Entities.Usuario GetOne(int ID)
+        {
+            return(UsuarioData.GetOne(ID));
+        }
+
+        public List<Usuario> GetAll()
+        {
+            return(UsuarioData.GetAll());
+        }
+
+        public void Save()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Delete()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public UsuarioLogic()
+        {
+            UsuarioData = new Data.Database.UsuarioAdapter();
+        }
     }
 }
