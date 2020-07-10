4490e41b03ab08b2e1a679616631591660cc4972
Tomás Cervillera <cervilleratomas@outlook.com>
2020-07-10 11:31:00 -0300

    Agregados formularios de Plan.

---
+++ Lab05/UI.Desktop/PlanDesktop.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Business.Logic;
+using Business.Entities;
+
+namespace UI.Desktop
+{
+    public partial class PlanDesktop : ApplicationForm
+    {
+        //Propiedades
+        private Business.Entities.Plan _PlanActual;
+        public Business.Entities.Plan PlanActual { get => _PlanActual; set => _PlanActual = value; }
+
+        //Constructores
+        public PlanDesktop()
+        {
+            InitializeComponent();
+        }
+        public PlanDesktop(ModoForm modo) : this()
+        {
+            Modo = modo;
+        }
+        public PlanDesktop(int ID, ModoForm modo) : this()
+        {
+            Modo = modo;
+            PlanActual = new PlanLogic().GetOne(ID);
+            MapearDeDatos();
+        }
+
+        //Métodos
+        public override void MapearDeDatos()
+        {
+            this.txtID.Text = this.PlanActual.ID.ToString();          
+            this.txtDescripcion.Text = this.PlanActual.Descripcion;          
+
+            if (Modo == ModoForm.Alta || Modo == ModoForm.Modificacion)
+            {
+                this.btnAceptar.Text = "Guardar";
+            }
+            else if (Modo == ModoForm.Baja)
+            {
+                this.btnAceptar.Text = "Eliminar";
+            }
+            else if (Modo == ModoForm.Consulta)
+            {
+                this.btnAceptar.Text = "Aceptar";
+            }
+
+        }
+        public override void MapearADatos()
+        {
+
+            if (Modo == ModoForm.Alta)
+            {
+                PlanActual = new Business.Entities.Plan();
+            }
+            if (Modo == ModoForm.Alta || Modo == ModoForm.Modificacion)
+            {
+                PlanActual.Descripcion = txtDescripcion.Text;
+
+                switch (Modo)
+                {
+                    case ModoForm.Alta:
+                        {
+                            PlanActual.State = BusinessEntity.States.New;
+                            break;
+                        }
+                    case ModoForm.Modificacion:
+                        {
+                            PlanActual.State = BusinessEntity.States.Modified;
+                            break;
+                        }
+                    case ModoForm.Consulta:
+                        {
+                            PlanActual.State = BusinessEntity.States.Unmodified;
+                            break;
+                        }
+                    case ModoForm.Baja:
+                        {
+                            PlanActual.State = BusinessEntity.States.Deleted;
+                            break;
+                        }
+                }
+            }
+        }
+        public override void GuardarCambios()
+        {
+            MapearADatos();
+            new PlanLogic().Save(PlanActual);
+        }
+        public override bool Validar()
+        {
+            foreach (Control oControls in this.tableLayoutPanel1.Controls) // Buscamos en cada TextBox de nuestro Formulario.
+            {
+                if (oControls is TextBox & oControls.Text == String.Empty & oControls.Name != "txtID") // Verificamos que no este vacio exceptuando al txtID porque se asigna automáticamente.
+                {
+                    Notificar("Hay al menos un campo vacío. Por favor, completelo/s. ", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return (false);
+                }
+            }
+            return (true);
+        }
+        
+        //Eventos
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            if (Validar()) 
+            {
+                GuardarCambios();
+                Close();
+            }
+        }
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+
+
+
+    }
+}
---
+++ Lab05/UI.Desktop/Planes.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Business.Entities;
+using Business.Logic;
+
+namespace UI.Desktop
+{
+    public partial class Planes : Form
+    {
+        //Constructor
+        public Planes()
+        {
+            InitializeComponent();
+            GenerarColumnas();
+        }
+
+        //Métodos
+        private void GenerarColumnas()
+        {
+            this.dgvPlanes.AutoGenerateColumns = false;
+
+            DataGridViewTextBoxColumn colID = new DataGridViewTextBoxColumn();
+            colID.Name = "id";
+            colID.HeaderText = "ID";
+            colID.DataPropertyName = "ID";
+            colID.DisplayIndex = 0;
+            this.dgvPlanes.Columns.Add(colID);
+
+            DataGridViewTextBoxColumn colDescripcion = new DataGridViewTextBoxColumn();
+            colDescripcion.Name = "descripcion";
+            colDescripcion.HeaderText = "Descripcion";
+            colDescripcion.DataPropertyName = "Descripcion";
+            colDescripcion.DisplayIndex = 1;
+            this.dgvPlanes.Columns.Add(colDescripcion);
+
+        }
+        public void Listar()
+        {
+            PlanLogic el = new PlanLogic();
+            this.dgvPlanes.DataSource = el.GetAll();
+        }
+
+        //Eventos
+        private void Planes_Load(object sender, EventArgs e)
+        {
+            Listar();
+        }
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            Listar();
+        }
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void tsbNuevo_Click(object sender, EventArgs e)
+        {
+            PlanDesktop formPlan = new PlanDesktop(ApplicationForm.ModoForm.Alta);
+            formPlan.ShowDialog();
+            this.Listar();
+        }
+        private void tsbEditar_Click(object sender, EventArgs e)
+        {
+            int ID = ((Business.Entities.Plan)this.dgvPlanes.SelectedRows[0].DataBoundItem).ID;
+            PlanDesktop formPlan = new PlanDesktop(ID, ApplicationForm.ModoForm.Modificacion);
+            formPlan.ShowDialog();
+            this.Listar();
+        }
+        private void tsbEliminar_Click(object sender, EventArgs e)
+        {
+            
+            if (MessageBox.Show("Está seguro de que desea eliminar esta plan? ", "Atención", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                int ID = ((Business.Entities.Plan)this.dgvPlanes.SelectedRows[0].DataBoundItem).ID;
+                new PlanLogic().Delete(ID);
+                this.Listar();
+            }
+        }
+    }
+}
